69344baf4178fd29e7757c11a29ae26d2591c242
Sam <meysam.or.us@gmail.com>
2023-03-18 16:06:19 +0330

    Create Car model and corresonding views

--- carSite/cars/models.py
+++ carSite/cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=30)
+    year = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"The car is of brand {self.brand} manufactured in {self.year}"
--- carSite/cars/views.py
+++ carSite/cars/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 def list_cars(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    all_cars_dict = {'all_cars': all_cars}
+
+    return render(request, 'cars/list.html', context=all_cars_dict)
 
 def add_cars(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = request.POST['year']
+        models.Car.objects.create(brand=brand, year=year)
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 def remove_cars(request):
-    return render(request, 'cars/remove.html')
+    if request.POST:
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print("pk not found")
+            return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/remove.html')
