ab391c3611450f58d05506c1dfa6671f38a51cb5
sam <meysam.or.us@gmail.com>
2023-03-19 13:01:47 -0400

    Create admin class

--- carSite/cars/admin.py
+++ carSite/cars/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Time info', {'fields': ['year']}),
+        ('Car info', {'fields': ['brand']})
+    ]
+
+admin.site.register(Car, CarAdmin)
