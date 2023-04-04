dddaad85caccece46c962049a7c958f253781cb4
Sam <meysam.or.us@gmail.com>
2023-04-04 08:55:02 +0330

    Create new env

--- django_env/Scripts/activate.bat
+++ django_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\projects\Django_project\django_env
+set VIRTUAL_ENV=C:\Users\m.rezaei\Documents\projects\Django_project\Django_env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(django_env) %PROMPT%
+set PROMPT=(Django_env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
